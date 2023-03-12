0797f3d12a9939d4a5d5a6e01d419800061a4917
CyanUnderscore <106006219+CyanUnderscore@users.noreply.github.com>
2023-03-12 21:11:52 +0100

    adding random number generation

--- rustlearn/src/main.rs
+++ rustlearn/src/main.rs
@@ -1,6 +1,10 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
+
+    let secret_number : i32 = rand::thread_rng().gen_range(1..=100);
+
     println!("Guess the number!");
 
     println!("Please input your guess.");
